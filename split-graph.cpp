67b419d79221a8a7da41d405ec7dd3314de74a66
Karl-Johan Karlsson <creideiki@ferretporn.se>
2004-09-24 14:11:23 +0000

    Fluff.

--- split-graph.cpp
+++ split-graph.cpp
@@ -1,21 +1,42 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
+
+class RFC;
+
+vector<RFC *> rfcs;
 
 class RFC
 {
 public:
-   RFC(string number, string attributes) :
-      live(true), _number(number), _attributes(attributes),
+   RFC(int number, string attributes) :
+      live(true), _number(number), _attributes(attributes)
    {}
 
+   void add_child(int number, string attributes)
+   {
+      children.push_back(pair<RFC *, string>(rfcs[number], attributes));
+   }
+
+   void add_parent(int number)
+   {
+      parents.push_back(rfcs[number]);
+   }
+
    bool live;
-   string _number, _attributes;
-   vector<RFC *> children;
+   int _number;
+   string _attributes;
+   vector< pair<RFC *, string> > children;
    vector<RFC *> parents;
 };
+
+ostream& operator<<(ostream &ost, const RFC &rfc)
+{
+   return ost;
+}
 
 int main(int argc, char *argv[])
 {
@@ -30,9 +51,9 @@ int main(int argc, char *argv[])
    istringstream iss(argv[1]);
    iss >> max_rfc_num;
 
-   vector<RFC *> rfcs(max_rfc_num, NULL);
+   rfcs.resize(max_rfc_num + 1, NULL);
 
-   while(true)
+   while(!cin.eof())
    {
       string type;
       cin >> type;
@@ -44,22 +65,57 @@ int main(int argc, char *argv[])
 
          int num;
          istringstream iss(number);
-         number >> num;
+         iss >> num;
 
-         //Läs resten, typ
-         cin.getline();
          string attributes;
+         getline(cin, attributes);
 
-         RFC *rfc = new RFC(number, attributes);
+         RFC *rfc = new RFC(num, attributes);
          rfcs[num] = rfc;
       }
       else if(type == "edge")
       {
+         string from, arrow, to;
+         cin >> from >> arrow >> to;
+
+         int parent, child;
+         istringstream iss(from);
+         iss >> parent;
+         istringstream iss2(to);
+         iss2 >> child;
+
+         if(!rfcs[parent])
+         {
+            cout << "Edge from " << from << " to " << to << " has a non-existant parent." << endl;
+            exit(1);
+         }
+
+         if(!rfcs[child])
+         {
+            cout << "Edge from " << from << " to " << to << " has a non-existant child." << endl;
+            exit(1);
+         }
+
+         string attributes;
+         getline(cin, attributes);
+
+         rfcs[parent]->add_child(child, attributes);
+         rfcs[child]->add_parent(parent);
       }
       else
       {
-         cout << "Unknown record type \"" << type "\"" << endl;
+         if(cin.eof())
+            break;
+         cout << "Unknown record type \"" << type << "\"" << endl;
          exit(1);
       }
+   }
+
+   for(int i = 0; i <= max_rfc_num; ++i)
+   {
+      if(!rfcs[i]->live)
+         continue;
+
+      
    }
 }
